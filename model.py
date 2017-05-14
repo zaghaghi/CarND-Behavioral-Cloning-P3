5ab87f4923d585d622a345e1d54a7f5a3036231a
Javid <javid@douran.com>
2017-05-14 16:58:41 +0430

    changes for titanx

--- model.py
+++ model.py
@@ -10,17 +10,19 @@ import cv2
 os.environ['KERAS_BACKEND'] = 'tensorflow'
 from keras.models import Sequential
 from keras.layers import Dense, Flatten, Lambda, Dropout
-from keras.layers.convolutional import Convolution2D
+from keras.layers.convolutional import Convolution2D, Cropping2D
 from keras.layers.pooling import MaxPool2D
+
 
 class Data:
     '''
     This class contains image, steering and other usefull information
     '''
     def __init__(self, image_path, steering, flip=False):
-        self.image_path = image_path
+        self.image_path = image_path.replace('\\', '/')
         self.steering = steering
         self.flip = flip
+
 
 class DataGenerator:
     '''
@@ -51,20 +53,20 @@ class DataGenerator:
                     # center image and its steering
                     data.append(Data(os.path.join(item, row[0]), float(row[3])))
                     # left image and its steering
-                    data.append(Data(os.path.join(item, row[1]), \
-                                    float(row[3]) + self.steering_correction))
+                    data.append(Data(os.path.join(item, row[1]),
+                                     float(row[3]) + self.steering_correction))
                     # right image and its steering
-                    data.append(Data(os.path.join(item, row[2]), \
-                                    float(row[3]) - self.steering_correction))
+                    data.append(Data(os.path.join(item, row[2]),
+                                     float(row[3]) - self.steering_correction))
 
                     # center flipped image and its steering
                     data.append(Data(os.path.join(item, row[0]), float(row[3]), True))
                     # left flipped image and its steering
-                    data.append(Data(os.path.join(item, row[1]), \
-                                    float(row[3]) + self.steering_correction, True))
+                    data.append(Data(os.path.join(item, row[1]),
+                                     float(row[3]) + self.steering_correction, True))
                     # right flipped image and its steering
-                    data.append(Data(os.path.join(item, row[2]), \
-                                    float(row[3]) - self.steering_correction, True))
+                    data.append(Data(os.path.join(item, row[2]),
+                                     float(row[3]) - self.steering_correction, True))
         data = shuffle(data)
         self.train_data, self.valid_data =\
             train_test_split(data, test_size=valid_split)
@@ -82,13 +84,14 @@ class DataGenerator:
                 batch_images_data = []
                 for d in batch_data:
                     img_data = cv2.imread(d.image_path)
+                    steering = d.steering
                     if d.flip:
-                        batch_measurements.append(-d.steering)
-                        img_data = np.fliplr(img_data)
-                    else:
-                        batch_measurements.append(d.steering)
-                    batch_images_data.append(img_data)
+                        steering = -d.steering
+                        # img_data = np.fliplr(img_data)
+                        img_data = cv2.flip(img_data, 1)
 
+                    batch_measurements.append(steering)
+                    batch_images_data.append(img_data)
                 yield shuffle(np.array(batch_images_data), np.array(batch_measurements))
 
     def get_train_data(self, batch_size=32):
@@ -107,6 +110,7 @@ class DataGenerator:
         '''returns validation samples size'''
         return len(self.valid_data)
 
+
 class Model:
     '''
     This class contains model implementation in keras
@@ -120,7 +124,9 @@ class Model:
     def build_model(self):
         ''' Build model '''
         self.model = Sequential()
-        self.model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+        self.model.add(Cropping2D(cropping=((50, 20), (0, 0)),
+                                  input_shape=(160, 320, 3)))
+        self.model.add(Lambda(lambda x: x / 255.0 - 0.5))
         self.model.add(Convolution2D(32, kernel_size=(3, 3), activation='relu'))
         self.model.add(MaxPool2D(pool_size=(2, 2)))
         self.model.add(Convolution2D(64, kernel_size=(3, 3), activation='relu'))
@@ -131,31 +137,31 @@ class Model:
         self.model.add(Dense(64))
         self.model.add(Dense(1))
 
-    def train_save(self, output, loss='mse', optimizer='adam', nb_epoch=7):
+    def train_save(self, output, loss='mse', optimizer='adam', epochs=7):
         '''
         Train the model and save it to output filename
         '''
-        train_generator = self.data_gen.get_train_data()
-        valid_generator = self.data_gen.get_valid_data()
+        train_generator = self.data_gen.get_train_data(batch_size=128)
+        valid_generator = self.data_gen.get_valid_data(batch_size=128)
 
         self.model.compile(loss=loss, optimizer=optimizer)
-        self.model.fit_generator(train_generator, \
-            steps_per_epoch=self.data_gen.get_train_size(), \
-            validation_data=valid_generator, \
-            validation_steps=self.data_gen.get_valid_size(), epochs=nb_epoch)
+        self.model.fit_generator(train_generator,
+                                 steps_per_epoch=self.data_gen.get_train_size(),
+                                 validation_data=valid_generator,
+                                 validation_steps=self.data_gen.get_valid_size(),
+                                 epochs=epochs)
         self.model.save(output)
-
 
 
 def main():
     '''
     main function
     '''
-    data_dir = [os.path.join('data', d) for d in os.listdir('data') \
+    data_dir = [os.path.join('data', d) for d in os.listdir('data')
                 if os.path.isdir(os.path.join('data', d))]
     data_gen = DataGenerator(data_dir)
     model = Model(data_gen)
-    model.train_save('model.v2.h5')
+    model.train_save('model.v2.h5', epochs=4)
 
 if __name__ == '__main__':
     main()
